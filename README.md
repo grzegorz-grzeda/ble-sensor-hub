4695b2388937531fafcc64be4351dc4a5fd77e66
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2021-11-29 15:54:37 +0100

    Minor refactoring, renaming & readme

---
+++ README.md
@@ -0,0 +1,41 @@
+# Simple BLE Sensor handler with MQTT
+This is a simple project intending to capture functionality of the 
+[ATC1441](https://github.com/atc1441/ATC_MiThermometer) sensors and push data through a computer (desktop, RPi, etc.) 
+to the MQTT broker.
+
+## Setup
+After downloading the project, install the `python3`, `python3-pip` and `bluepy`. 
+If you won't add needed capabilities, run the script `sudo ./sensor.py`. Otherwise run it normally `./sensor.py`.
+
+For the first time, the program will create a `config.json` file. You need to fill it with proper entries.
+
+You also need to run a simple web server, which will return the dynamic configuration for rest of operation.
+The server should serve a simple GET request on the path defined in the `api_server` config entry in `config.json`.
+The configuration is as follows (example):
+```
+{
+    "whitelist":{
+        "a4:c1:38:WW:YY:ZZ": "/room1",
+        "a4:c1:38:JJ:KK:LL": "/room10"
+    },
+    "scan_interval": "10",
+    "sleep_interval": "20",
+    "mqtt_broker_ip":"<MQTT_BROKER_IP_HERE>",
+    "mqtt_broker_port": "<MQTT_BROKER_PORT_HERE>"
+}
+```
+
+## Operation
+When everything is set up:
+- the program starts,
+- fetches configuration from the defined server location,
+- connects to the MQTT broker
+- periodically scans the BLE area for devices
+- if finds a device from the whitelist:
+   - it presumes its a ATC1441, 
+   - parses the service data,
+   - packs it and sends to the defined MQTT topic
+
+# Copyright
+Under MIT license
+&copy; 2021 G2Labs Grzegorz Grzęda
--- sensor.py
+++ sensor.py
@@ -1,37 +1,60 @@
 #!/usr/bin/python3
 from bluepy.btle import Scanner as ble_scanner
 import paho.mqtt.client as mqtt_client
-import requests
-import json
-import sys
-from logging import info, error, basicConfig, INFO
+from requests import get as get_request
+from json import load as load_json_from_file, loads as load_json_from_string, dump as dump_json_to_file
+from sys import exit
+from logging import info as i, error as e, basicConfig as basic_logging_config, INFO
 from time import sleep
 
 CONFIG = "config.json"
 
 
 def configure_logging():
-    basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=INFO)
+    """
+    Configure logging utility with proper formatting
+    """
+    basic_logging_config(format="%(asctime)s [%(levelname)s] %(message)s", level=INFO)
+
+
+def create_system_configuration_template():
+    """
+    Creates a template for system configration
+    """
+    config = {
+        "api_server": "http://<API_SERVER_URL_HERE>",
+        "mqtt_id": "<MQTT_UNIQUE_ID_HERE>"
+    }
+    i("Creating template config file")
+    with open(CONFIG, "w") as config_json:
+        dump_json_to_file(config, config_json)
 
 
 def get_system_configuration():
+    """
+    Get the basic configuration
+    """
     with open(CONFIG) as config_json:
-        return json.load(config_json)
+        return load_json_from_file(config_json)
 
 
 def get_measurement_configuration(api_server_url):
-    r = requests.get(api_server_url)
+    """
+    Get rest of configuration from api server
+    """
+    r = get_request(api_server_url)
     if(r.status_code == 200):
-        return json.loads(r.text)
+        return load_json_from_string(r.text)
     else:
         raise Exception(f"Couldn't fetch configuration - HTTP status: {r.status_code}")
 
 
 def connect_mqtt(broker, port, id):
+    """
+    Connect to MQTT broker
+    """
     def on_connect(_client, _userdata, _flags, rc):
-        if rc == 0:
-            info("Connected to MQTT Broker!")
-        else:
+        if rc != 0:
             raise Exception(f"Counldn't connect to MQTT Broker {broker}:{port}")
     client = mqtt_client.Client(id)
     client.on_connect = on_connect
@@ -40,6 +63,9 @@ def connect_mqtt(broker, port, id):
 
 
 def handle_single_device(dev, devices, mqtt):
+    """
+    Handle a single entry of BLE Scanning
+    """
     address = dev.addr.lower()
     rssi = dev.rssi
     if address in devices:
@@ -48,12 +74,15 @@ def handle_single_device(dev, devices, mqtt):
                 topic = devices[address]
                 humidity = int(value[20:22], 16)
                 temperature = int(value[16:20], 16) / 10
-                info(f"=> {address} @ {rssi} dBm, MQTT: {topic}")
+                i(f"=> {address} @ {rssi} dBm, MQTT: {topic}")
                 data_to_send = f"{temperature};{humidity}"
                 mqtt.publish(f"{topic}", data_to_send)
 
 
 def main():
+    """
+    The main code
+    """
     configure_logging()
     while(True):
         try:
@@ -68,10 +97,14 @@ def main():
                     handle_single_device(dev, config["whitelist"], mqtt)
                 sleep(float(config["sleep_interval"]))
         except FileNotFoundError as exp:
-            error(f"Couldn't find configuration file '{exp.filename}'!")
-            sys.exit(-1)
+            e(f"Couldn't find configuration file '{exp.filename}'!")
+            create_system_configuration_template()
+            exit(-1)
+        except KeyboardInterrupt:
+            i("Program interrupted by user")
+            exit(0)
         except Exception as exp:
-            error(exp)
+            e(exp)
 
 
 if __name__ == "__main__":
