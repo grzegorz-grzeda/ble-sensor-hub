3b793addce30bba56cef32da6a87a99920b11307
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2022-01-20 17:42:29 +0100

    Fixed a bug

--- sensor-hub.py
+++ sensor-hub.py
@@ -15,7 +15,8 @@ def get_sleep_interval(sleep_interval_from_config):
 
 def main():
     sh = logging.StreamHandler()
-    sh.format('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    sh.format(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    sh.setLevel(logging.INFO)
     logger = logging.getLogger('sensor-hub')
     logger.setLevel(logging.INFO)
     logger.addHandler(sh)
